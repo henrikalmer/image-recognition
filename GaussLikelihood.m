12a9193d8e9324f4a7776c52d29a7c94d462bd67
Henrik Almér <henrik.almer@gmail.com>
2014-04-04 15:10:56 +0200

    Enable toggling between RGB and HSV colour models

--- GaussLikelihood.m
+++ GaussLikelihood.m
@@ -1,9 +1,20 @@
-function lvals = GaussLikelihood(xs, mu, Sigma)
+function lvals = GaussLikelihood(xs, mu, Sigma, m)
 %GAUSSLIKELIHOOD Calculates likelyhood that vector xs belongs to mu+Sigma
 %   xs contains RGB pixel data. mu and Sigma are the mean vector and
 %   covariance matrix describing the probability that a pixel belongs to a
 %   certain class, given its RGB data. Returns an array of pixel by pixel
-%   likelyhoods.
+%   likelyhoods. Choose colour model RGB or HSV by setting m.
+
+if strcmp(m, 'HSV')
+    xs_hsv = rgb2hsv(xs);
+    xs = zeros(size(xs_hsv,1), 4);
+    for i=1:size(xs,1)
+        xs(i,1) = cos(xs_hsv(i,1));
+        xs(i,2) = sin(xs_hsv(i,1));
+        xs(i,3) = xs_hsv(i,2);
+        xs(i,4) = xs_hsv(i,3);
+    end
+end
 
 lvals = mvnpdf(double(xs), mu, Sigma);
 
--- TrainColourModel.m
+++ TrainColourModel.m
@@ -1,20 +1,32 @@
-function [mu, Sigma] = TrainColourModel(DirName, n)
+function [mu, Sigma] = TrainColourModel(DirName, n, m)
 %TRAINCOLOURMODEL Returns mean vector and covariance matrix for ims in Dir
 %   Extracts the central pixels from each image in DirName and calculates
-%   the mean vector and covariance matrix of those pixels.
+%   the mean vector and covariance matrix of those pixels. Choose colour
+%   model RGB or HSV by setting m.
 
 selector = [DirName, '/*.jpg'];
 im_files = dir(selector);
 ni = min(length(im_files), n);
 addpath(DirName);
 
-RGB = [];
+IMS = [];
 for i=1:ni
     im = GrabCenterPixels(im_files(i).name, .2);
-    RGB = [RGB; reshape(im, [size(im,1)*size(im,2), 3])];
+    im_data = reshape(im, [size(im,1)*size(im,2), 3]);
+    if strcmp(m, 'HSV')
+        im_hsv = rgb2hsv(im_data);
+        im_data = zeros(size(im_hsv,1), 4);
+        for i=1:size(im_data,1)
+            im_data(i,1) = cos(im_hsv(i,1));
+            im_data(i,2) = sin(im_hsv(i,1));
+            im_data(i,3) = im_hsv(i,2);
+            im_data(i,4) = im_hsv(i,3);
+        end
+    end
+    IMS = [IMS; im_data];
 end
-mu = mean(RGB);
-Sigma = cov(double(RGB));
+mu = mean(IMS);
+Sigma = cov(double(IMS));
 
 end
 
